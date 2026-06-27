e03af9ecd7026899d07284c71196ee0d7beb09f3
yonatan bohorquez <xdolmario@gmail.com>
2026-06-27 10:04:45 -0500

    Documentar plan de pruebas y bugs encontrados en sesion de testing

--- App.js
+++ App.js
@@ -109,6 +109,7 @@ export default function App() {
             headerTintColor: '#F59E0B',
             headerTitle: 'Buscar'
           }} />
+          
       </Tab.Navigator>
       <StatusBar style="light" />
     </NavigationContainer>
